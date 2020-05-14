e38ab934af7abff17ddc614bb9b72934c29edbf7
Tomasz Wołyniec <t.wolyniec@tlen.pl>
2020-05-14 10:01:57 +0200

    commit

--- src/lambdaTutorial/Calculator.java
+++ src/lambdaTutorial/Calculator.java
@@ -35,7 +35,7 @@ public class Calculator {
     public static void main(String... args) {
 
         Calculator myApp = new Calculator();
-        IntegerMath addition = Integer::sum;
+        IntegerMath addition = (a, b) -> a + b;
         IntegerMath subtraction = (a, b) -> a - b;
         System.out.println("40 + 2 = " +
                 myApp.operateBinary(40, 2, addition));
